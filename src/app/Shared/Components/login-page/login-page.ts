3a06f9430da79920971270b1f568ec7af0c5a08b
ksabry97 <isabryex@gmail.com>
2025-08-10 18:50:16 +0300

    login container

--- src/app/Shared/Components/login-page/login-page.ts
+++ src/app/Shared/Components/login-page/login-page.ts
@@ -10,6 +10,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login-page',
   imports: [
@@ -18,12 +19,36 @@ import {
     PasswordInput,
     NzButtonModule,
     ReactiveFormsModule,
+    CommonModule,
   ],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
   loginForm!: FormGroup;
+
+  content = [
+    {
+      label: 'Patient Management',
+      content: 'Comprehensive patient records and care coordination',
+      color: '#e0fff2',
+    },
+    {
+      label: 'Real-time Monitoring',
+      content: 'Live patient vitals and automated alerts',
+      color: '#ebfaff',
+    },
+    {
+      label: 'Staff Coordination',
+      content: 'Seamless communication between departments',
+      color: '#e0fff2',
+    },
+    {
+      label: 'Appointment Scheduling',
+      content: 'Intelligent scheduling and resource management',
+      color: '#ebfaff',
+    },
+  ];
   constructor(private readonly fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
