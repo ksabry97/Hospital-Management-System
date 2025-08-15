f3eb1f31f446b343450cca0b0d8e4532154ec968
momen <monosh89@gmail.com>
2025-08-15 19:47:52 +0300

    remove header , footer of modal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,11 +8,21 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import { IconDefinition } from '@ant-design/icons-angular';
+
+// Create array of all icons
+const icons: IconDefinition[] = Object.keys(AllIcons).map(
+  (key) => (AllIcons as any)[key]
+);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideAnimations(),
+    { provide: NZ_ICONS, useValue: icons },
   ],
 };
