aa647ee7d762e43e22a42ecb98ad13d29845fbfd
ksabry97 <isabryex@gmail.com>
2025-08-15 12:15:32 +0300

    re-structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './Shared/Components/login-page/login-page';
 import { PageNotFound } from './Shared/Components/page-not-found/page-not-found';
-import { Clincs } from './clincs/pages/clincs/clincs';
+import { Clincs } from './Modules/Staff-Managment/pages/clincs/clincs';
 
 export const routes: Routes = [
   {
